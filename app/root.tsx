c7f3cfa2ce969bc20fb5ddb870cdefc69b2280ed
rohanawaz <rohanawaz118@gmail.com>
2022-05-02 10:39:24 -0900

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,16 @@
+import { Outlet, LiveReload } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
+      </head>
+      <body>
+        <Outlet />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
